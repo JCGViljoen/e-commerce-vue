f01f22a5b69054e7002bb9fb35e39f8702d266b1
JCGViljoen <jcgviljone51@gmail.com>
2023-06-28 16:22:49 +0200

    commit message

---
+++ src/components/ProductsComp.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+<h2>{{ prduct.name }}</h2>
+<p>Brand: {{ product.brand }}</p>
+<p>Description: {{product.description}}</p>
+<p>Price: {{product.Price}}</p>
+<img src="product.image" alt="product.name">
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            product: {
+                type: Object,
+                required: true
+            }
+        }
+        
+    };
+</script>
+
+<style scoped>
+
+</style>
